a2f49ada90651db266bbc4ff2e580b9e3c56e0fe
mayamouse <cooperpontea@gmail.com>
2023-05-18 13:46:18 -0400

    began to write up markdown file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,22 +1,53 @@
-//MIT
+//MIT MIT
 //https://img.shields.io/badge/License-MIT-yellow.svg)
 //https://opensource.org/licenses/MIT
 
-//Apache
-//
-//
+//Apache_2.0
+//https://img.shields.io/badge/License-Apache_2.0-blue.svg
+//https://opensource.org/licenses/Apache-2.0
 
-//Mozilla
-//
-//
+//Mozilla MPL 2.0
+//(https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)
+//(https://opensource.org/licenses/MPL-2.0)
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
+function renderLicenseBadge(license) {
+  let badge = "";
+
+  if(license = "MIT") {
+    badge = "![License Badge](https://img.shields.io/badge/License-MIT-yellow.svg)";
+  }
+  else if (license = "MPL 2.0") {
+    badge = "![License Badge](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)";
+
+  }
+  else if (license = "Apache_2.0") {
+    badge = "![License Badge](https://opensource.org/licenses/Apache-2.0)";
+
+  }
+  return badge;
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink = "";
+
+  if(license = "MIT") {
+    licenseLink = "![License Badge](https://img.shields.io/badge/License-MIT-yellow.svg)";
+  }
+  else if (license = "MPL 2.0") {
+    licenseLink = "![License Badge](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)";
+
+  }
+  else if (license = "Apache_2.0") {
+    licenseLink = "![License Badge](https://opensource.org/licenses/Apache-2.0)";
+
+  }
+  return licenseLink;
+}
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
